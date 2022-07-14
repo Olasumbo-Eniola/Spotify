de546eb36ea7333df3e31cf63d20f3f6181abc80
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2022-07-14 03:20:12 +0100

    Setting up environment

---
+++ src/components/Signin/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "./index.module.scss";
+
+export default function SignIn() {
+  return <></>;
+}
---
+++ src/components/Signup/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./index.module.scss"
+
+export default function SignUp(){
+    return(
+        <>
+        hii
+        </>
+    )
+}
